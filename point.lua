ce1dcd1b7eb2ab2d5e4a63dde28fdd952b976805
Tommy Hansen <tvhansen33@gmail.com>
2012-12-28 16:27:57 +0100

    added some comments

--- point.lua
+++ point.lua
@@ -1,6 +1,9 @@
 Point = {}
 Point.__index = Point
 
+-- Creates a new Point instance
+-- _x: X-coordinate of the instance
+-- _y: Y-coordiante of the instance
 function Point.create( _x, _y )
 	local self = setmetatable({}, Point)
 	self.x = _x
@@ -8,8 +11,12 @@ function Point.create( _x, _y )
 	return self
 end
 
-function Point:rotate( _p, _angle )
-	local a = ((_angle-1)%4) + 1
+-- Rotates the point 90 degrees counter-clockwise around
+-- a specified number of times around a given point
+-- _p: The point to rotate about
+-- _n: The number of times to rotate 90 degress
+function Point:rotate( _p, _n)
+	local a = ((_n-1)%4) + 1
 	local sin,cos
 	if a == 1 then
 		sin = 1
@@ -24,13 +31,14 @@ function Point:rotate( _p, _angle )
 		sin = 0
 		cos = 1
 	end
-	local tmpx = self.x
-	self.x = _p.x + cos*(self.x - _p.x) + sin*(self.y - _p.y)
-	self.y = _p.y - sin*(tmpx - _p.x) + cos*(self.y - _p.y)
-	--self.x = _p.x + sin*(self.y - _p.y)
-	--self.y = _p.y - cos*(tmpx - _p.x)
+	local dx, dy = (self.x-_p.x), (self.y-_p.y)
+	self.x = _p.x + cos*dx + sin*dy
+	self.y = _p.y - sin*dx + cos*dy
 end
 
+
+-- Tests whether or not the point instance is in the board grid
+-- Returns true if inside grid, false otherwise
 function Point:checkBounds( )
 	if self.x <= 0 or self.x > GRID_WIDTH or self.y <= 0 or self.y > GRID_HEIGHT then
 		return false
@@ -38,10 +46,16 @@ function Point:checkBounds( )
 	return true
 end
 
+-- Simply returns the coordinates of the point
+-- Order is x, y
 function Point:coords( )
 	return self.x, self.y
 end
 
+-- Compares two given point instances
+-- _p1: The first point instance
+-- _p2: The second point instance
+-- True if equal, false otherwise
 function Point.compare( _p1, _p2 )
 	if _p1.x == _p2.x and _p1.y == _p2.y then
 		return true
@@ -49,6 +63,8 @@ function Point.compare( _p1, _p2 )
 	return false
 end
 
+-- Returns a string description of the point
+-- Mainly (probably only) for debugging purposes
 function Point:print( )
 	return "("..self.x..", "..self.y..")"
 end
