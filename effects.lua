9fc33d9f290d4450a0515c53b2228bfbd1d6f2fa
Tommy Hansen <tvhansen33@gmail.com>
2012-12-28 15:59:51 +0100

    lidt mere

--- effects.lua
+++ effects.lua
@@ -3,13 +3,11 @@ local lg = love.graphics
 PoolUnit = {}
 PoolUnit.__index = PoolUnit
 
-function PoolUnit.create( _x, _y, _speed )
+function PoolUnit.create( _x, _y )
 	local self = setmetatable({}, PoolUnit)
 	self.x = _x
 	self.y = _y
-	self.active = true
-	self.frame = 3*SCALE
-	self.speed = _speed
+	self.radius = 3*SCALE
 	self.tick = 0
 	return self
 end
@@ -17,14 +15,14 @@ end
 function PoolUnit:update( dt )
 	self.tick = self.tick + WATER_SPEED*dt
 	if self.tick >= 1 then
-		self.frame = self.frame + 1
+		self.radius = self.radius + 1
 		self.tick = 0
 	end
 end
 
 function PoolUnit:draw( )
 	lg.setColor(99,168,183)
-	lg.circle("fill", self.x, self.y, self.frame, 10)
+	lg.circle("fill", self.x, self.y, self.radius, 10)
 	lg.setColor(255,255,255)
 end
 
@@ -36,12 +34,9 @@ function Pool.create( _x, _y )
 	self.x = _x
 	self.y = _y
 	self.active = true
-	self.frame = 0
 	self.units = {}
-	self.units[1] = PoolUnit.create(_x, _y, 4)
-	--for i=2,5 do
-	--	self.units[i] = PoolUnit.create(_x+math.random(-1,1)*math.random(1,40), _y+math.random(-10,10), 4+math.random(2,4))
-	--end
+	self.units[1] = PoolUnit.create(_x, _y)
+	-- add more units in some genius looking way
 	return self
 end
 
